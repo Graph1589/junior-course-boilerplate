b13bca8159793fd62f0a145c31f1e02f0999fefc
graph1589 <graph1589@gmail.com>
2021-10-22 17:11:14 +0300

    comments work out

--- src/index.js
+++ src/index.js
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import products from './products';
 
-const ProductsList = ({ products }) => (
-    <>
+const ProductsList = (props) => (
+  <>
     <h2>Список товаров</h2>
-    {products.map(({ id, name }) => (
-      <li key={id}>{name}</li>
-    ))}
-    </>
+    <ul>
+      {props.products.map(({ id, name }) => (
+        <li key={id}>{name}</li>
+      ))}
+    </ul>
+  </>
 );
 
 const init = () => {
-  console.log(products);
-const shownProductNum = 3;
-const shownProducts = products.filter((value, index) => index < shownProductNum);
-const container = document.getElementById('root');
+  const shownProductNum = 3;
+  const shownProducts = products.filter((value, index) => index < shownProductNum);
+  const container = document.getElementById('root');
 
-ReactDOM.render(
-<ProductsList
-  products={shownProducts}
-  />, container);
+  ReactDOM.render(
+    <ProductsList
+      products={shownProducts}
+    />, container);
 };
 
 init();
