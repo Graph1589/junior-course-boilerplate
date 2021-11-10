8a3fa9542db5bc250a30d1dfe6d95b6ca13b56c3
graph1589 <graph1589@gmail.com>
2021-11-10 19:50:35 +0300

    product card extend pure

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,9 +2,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import ProductItem from 'csssr-school-product-card';
-import LogRender from '../LogRender/LogRender';
 
-export default class ProductCard extends LogRender {
+// Bug, freeze app, if ProductCard extend LogRender.
+export default class ProductCard extends React.PureComponent {
   render() {
     return (
       <ProductItem {...this.props} />
